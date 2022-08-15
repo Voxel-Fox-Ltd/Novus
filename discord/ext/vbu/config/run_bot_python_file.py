eabc25c8356a1cea7d47a81ce0b3d2067e582a0d
Kae Bartlett <kae@voxelfox.co.uk>
2022-08-15 05:10:55 +0100

    [vbu] Remove unnecessary imports

--- discord/ext/vbu/config/run_bot_python_file.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import voxelbotutils.__main__
-parser = voxelbotutils.__main__.get_default_program_arguments()
-args = parser.parse_args(['run-bot', '.'])
-voxelbotutils.runner.run_bot(args)
--- discord/ext/vbu/config/website_frontend_content.py
+++ discord/ext/vbu/config/website_frontend_content.py
@@ -1,8 +1,4 @@
-from aiohttp.web import HTTPFound, Request, Response, RouteTableDef
-import aiohttp_session
-from aiohttp_jinja2 import template
-import discord
-from discord.ext import vbu
+from aiohttp.web import RouteTableDef
 
 
 routes = RouteTableDef()
