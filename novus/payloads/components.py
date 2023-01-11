a1659ac44762a1b3a7ae00bd98acbde4534fa75c
Kae Bartlett <kae@voxelfox.co.uk>
2023-01-11 18:16:38 +0000

    Add message, embed, emoji, and components to payloads I also started interaction but only so far as to type the message object.

---
+++ novus/payloads/components.py
@@ -0,0 +1,108 @@
+"""
+Copyright (c) Kae Bartlett
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, TypedDict, Literal, Union
+
+if TYPE_CHECKING:
+    from .emoji import PartialEmoji
+
+__all__ = (
+    'ComponentType',
+    'Button',
+    'SelectOption',
+    'SelectMenu',
+    'TextInput',
+    'ActionRow',
+)
+
+
+ComponentType = Literal[
+    1,  # Action row
+    2,  # Button
+    3,  # String select
+    4,  # Text input
+    5,  # User select
+    6,  # Role select
+    7,  # Mentionable select
+    8,  # Channel select
+]
+
+
+class _ButtonOptional(TypedDict, total=False):
+    emoji: PartialEmoji
+    custom_id: str
+    url: str
+    disabled: bool
+
+
+class Button(_ButtonOptional):
+    type: Literal[2]
+    style: Literal[1, 2, 3, 4, 5]
+
+
+class _SelectOptionOptional(TypedDict, total=False):
+    description: str
+    emoji: PartialEmoji
+    default: bool
+
+
+class SelectOption(_SelectOptionOptional):
+    label: str
+    value: str
+
+
+class _SelectMenuOptional(TypedDict, total=False):
+    options: list[SelectOption]
+    channel_types: list[int]
+    placeholder: str
+    min_values: int
+    max_values: int
+    disabled: bool
+
+
+class SelectMenu(_SelectMenuOptional):
+    type: Literal[3, 5, 6, 7, 8]
+    custom_id: str
+
+
+class _TextInputOptional(TypedDict, total=False):
+    min_length: int
+    max_length: int
+    required: bool
+    value: str
+    placeholder: str
+
+
+class TextInput(_TextInputOptional):
+    type: Literal[4]
+    custom_id: str
+    style: Literal[1, 2]
+    label: str
+
+
+ActionRowComponents = Union[
+    Button,
+    SelectMenu,
+    TextInput,
+]
+
+
+class ActionRow(TypedDict):
+    type: Literal[1]
+    components: list[ActionRowComponents]
---
+++ novus/payloads/interaction.py
@@ -0,0 +1,49 @@
+"""
+Copyright (c) Kae Bartlett
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal, TypedDict
+
+if TYPE_CHECKING:
+    from ._snowflake import Snowflake
+    from .user import User, GuildMember
+
+__all__ = (
+    'InteractionType',
+    'MessageInteraction',
+)
+
+
+InteractionType = Literal[
+    1,  # PING
+    2,  # APPLICATION_COMMAND
+    3,  # MESSAGE_COMPONENT
+    4,  # APPLICATION_COMMAND_AUTOCOMPLETE
+    5,  # MODAL_SUBMIT
+]
+
+
+class _MessageInteractionOptional(TypedDict, total=False):
+    member: GuildMember
+
+
+class MessageInteraction(_MessageInteractionOptional):
+    id: Snowflake
+    type: InteractionType
+    name: str
+    user: User
