0b38118b655722748cd158e46742d39b3050163f
Kae Bartlett <kae@voxelfox.co.uk>
2023-01-13 15:07:06 +0000

    Add guild template payload

---
+++ novus/payloads/guild_template.py
@@ -0,0 +1,43 @@
+"""
+Copyright (c) Kae Bartlett
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, TypedDict
+
+if TYPE_CHECKING:
+    from ._snowflake import Snowflake, Timestamp
+    from .user import User
+    from .guild import Guild
+
+__all__ = (
+    'GuildTemplate',
+)
+
+
+class GuildTemplate(TypedDict):
+    code: str
+    name: str
+    description: Optional[str]
+    usage_count: int
+    creator_id: Snowflake
+    creator: User
+    created_at: Timestamp
+    updated_at: Timestamp
+    source_guild_id: Snowflake
+    serialized_source_guild: Guild
+    is_dirty: Optional[bool]
