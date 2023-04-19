693e465286efee0faeb7c8db46f8b461cfc74d89
Kae Bartlett <kae@voxelfox.co.uk>
2023-04-19 18:25:02 +0100

    Change keyword from config

--- novus/ext/database/database.py
+++ novus/ext/database/database.py
@@ -39,7 +39,7 @@ class Database(client.Plugin):
 
     CONFIG = {
         "database_dsn": "",
-        "database_connections": 10,
+        "database_max_connections": 10,
     }
     pool: asyncpg.Pool = None  # pyright: ignore
     _log: logging.Logger = logging.getLogger("database")  # pyright: ignore
@@ -68,16 +68,16 @@ class Database(client.Plugin):
         if not self.bot.config.database_dsn:
             self.log.error("Missing database DSN from config")
             return
-        if self.bot.config.database_connections is None:
-            self.bot.config.database_connections = 10
+        if self.bot.config.database_max_connections is None:
+            self.bot.config.database_max_connections = 10
 
         # Open a pool
         try:
             created = (
                 await asyncpg.create_pool(
                     self.bot.config.database_dsn,
-                    max_size=self.bot.config.database_connections,
-                    min_size=min(self.bot.config.database_connections, 10),
+                    max_size=self.bot.config.database_max_connections,
+                    min_size=min(self.bot.config.database_max_connections, 10),
                 )
             )
             if created is None:
