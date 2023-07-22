3bbbb0aa65ad0843995c6eaf60759f48e43e8c4b
Kae Bartlett <kae@voxelfox.co.uk>
2023-07-22 01:49:25 +0100

    Add event name aliases

--- novus/ext/client/event.py
+++ novus/ext/client/event.py
@@ -30,6 +30,13 @@ if TYPE_CHECKING:
         Message,
         MessageComponentData,
         TextChannel,
+        Guild,
+        AuditLog,
+        User,
+        GuildMember,
+        Emoji,
+        Sticker,
+        Role,
     )
 
 __all__ = (
@@ -107,9 +114,115 @@ class EventBuilder:
             lambda i: i.type == novus.InteractionType.message_component,
         )
 
+    # "Application command permissions update": None
+
+    # "Auto moderation rule create": None
+
+    # "Auto moderation rule update": None
+
+    # "Auto moderation rule delete": None
+
+    # "Auto moderation action execution": None
+
     @classmethod
-    def interaction(cls, func: W[Interaction]) -> EL:
-        return EventListener("INTERACTION_CREATE", func)
+    def channel_create(cls, func: W[Channel]) -> EL:
+        return EventListener("CHANNEL_CREATE", func)
+
+    @classmethod
+    def channel_update(cls, func: W2[Channel, Channel]) -> EL:
+        return EventListener("CHANNEL_UPDATE", func)
+
+    @classmethod
+    def channel_delete(cls, func: W[Channel]) -> EL:
+        return EventListener("CHANNEL_DELETE", func)
+
+    @classmethod
+    def thread_create(cls, func: W[Thread]) -> EL:
+        return EventListener("THREAD_CREATE", func)
+
+    @classmethod
+    def thread_update(cls, func: W[Thread]) -> EL:
+        return EventListener("THREAD_UPDATE", func)
+
+    # "Thread delete": None
+
+    # "Thread list sync": None
+
+    # "Thread member update": None
+
+    # "THREAD_MEMBERS_UPDATE": self._handle_thread_member_list_update
+
+    @classmethod
+    def guild_create(cls, func: W[Guild]) -> EL:
+        return EventListener("GUILD_CREATE", func)
+
+    @classmethod
+    def guild_update(cls, func: W2[Guild, Guild]) -> EL:
+        return EventListener("GUILD_UPDATE", func)
+
+    @classmethod
+    def guild_delete(cls, func: W[Guild]) -> EL:
+        return EventListener("GUILD_DELETE", func)
+
+    @classmethod
+    def audit_log_create(cls, func: W[AuditLog]) -> EL:
+        return EventListener("GUILD_AUDIT_LOG_ENTRY_CREATE", func)
+
+    @classmethod
+    def user_ban(cls, func: W2[Guild, User | GuildMember]) -> EL:
+        return EventListener("GUILD_BAN_ADD", func)
+
+    @classmethod
+    def user_unban(cls, func: W2[Guild, User]) -> EL:
+        return EventListener("GUILD_BAN_REMOVE", func)
+
+    @classmethod
+    def emojis_update(cls, func: W2[Guild, list[Emoji]]) -> EL:
+        return EventListener("GUILD_EMOJIS_UPDATE", func)
+
+    @classmethod
+    def stickers_update(cls, func: W2[Guild, list[Sticker]]) -> EL:
+        return EventListener("GUILD_STICKERS_UPDATE", func)
+
+    @classmethod
+    def guild_member_add(cls, func: W[GuildMember]) -> EL:
+        return EventListener("GUILD_MEMBER_ADD", func)
+
+    @classmethod
+    def guild_member_remove(cls, func: W2[Guild, User | GuildMember]) -> EL:
+        return EventListener("GUILD_MEMBER_REMOVE", func)
+
+    @classmethod
+    def guild_member_update(cls, func: W2[GuildMember, GuildMember]) -> EL:
+        return EventListener("GUILD_MEMBER_UPDATE", func)
+
+    @classmethod
+    def role_create(cls, func: W[Role]) -> EL:
+        return EventListener("GUILD_ROLE_CREATE", func)
+
+    @classmethod
+    def role_update(cls, func: W[Role, Role]) -> EL:
+        return EventListener("GUILD_ROLE_UPDATE", func)
+
+    @classmethod
+    def role_delete(cls, func: W[Role]) -> EL:
+        return EventListener("GUILD_ROLE_DELETE", func)
+
+    # "Guild scheduled event create": None
+
+    # "Guild scheduled event update": None
+
+    # "Guild scheduled event delete": None
+
+    # "Guild scheduled event user add": None
+
+    # "Guild scheduled event user remove": None
+
+    # "INTERACTION_CREATE": self._handle_interaction
+
+    # "INVITE_CREATE": self._handle_invite_create
+
+    # "INVITE_DELETE": self._handle_invite_delete
 
     @classmethod
     def message(cls, func: W[Message]) -> EL:
@@ -123,17 +236,33 @@ class EventBuilder:
     def message_delete(cls, func: W2[TextChannel, Message]) -> EL:
         return EventListener("MESSAGE_DELETE", func)
 
-    @classmethod
-    def channel_create(cls, func: W[Channel]) -> EL:
-        return EventListener("CHANNEL_CREATE", func)
+    # "Message delete bulk": None
 
-    @classmethod
-    def channel_update(cls, func: W2[Channel | None, Channel]) -> EL:
-        return EventListener("CHANNEL_UPDATE", func)
+    # "MESSAGE_REACTION_ADD": self._handle_message_reaction_add
 
-    @classmethod
-    def channel_delete(cls, func: W[Channel]) -> EL:
-        return EventListener("CHANNEL_DELETE", func)
+    # "MESSAGE_REACTION_REMOVE": self._handle_message_reaction_remove
+
+    # "MESSAGE_REACTION_REMOVE_ALL": self._handle_message_reaction_remove_all
+
+    # "MESSAGE_REACTION_REMOVE_EMOJI": self._handle_message_reaction_remove_all_emoji
+
+    # "PRESENCE_UPDATE": self._handle_presence_update
+
+    # "Stage instance create": None
+
+    # "Stage instance update": None
+
+    # "Stage instance delete": None
+
+    # "TYPING_START": self._handle_typing
+
+    # "User update": None
+
+    # "VOICE_STATE_UPDATE": self._handle_voice_state
+
+    # "Voice server update": None
+
+    # "Webhooks update": None
 
     @staticmethod
     def __call__(
