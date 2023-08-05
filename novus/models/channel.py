d2fad2dcbe69d6d5956290fa9f2ca2f2f06196d1
Kae Bartlett <kae@voxelfox.co.uk>
2023-08-05 14:50:48 +0100

    Add missing import from typing extensions

--- novus/models/channel.py
+++ novus/models/channel.py
@@ -20,6 +20,7 @@ from __future__ import annotations
 import asyncio
 import logging
 from typing import TYPE_CHECKING, Any, Literal, Type, overload
+from typing_extensions import Self
 
 from ..enums import ChannelType, PermissionOverwriteType
 from ..flags import Permissions
