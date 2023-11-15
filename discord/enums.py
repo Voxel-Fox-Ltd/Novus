fbaf68a3253834f6903027561f7e9a457d9959e2
George M <32426140+iHeroGH@users.noreply.github.com>
2023-11-15 12:28:46 -0800

    Add `gif` to StickerFormatType enum

--- discord/enums.py
+++ discord/enums.py
@@ -515,6 +515,7 @@ class StickerFormatType(Enum):
     png = 1
     apng = 2
     lottie = 3
+    gif = 4
 
     @property
     def file_extension(self) -> str:
@@ -523,6 +524,7 @@ class StickerFormatType(Enum):
             StickerFormatType.png: 'png',
             StickerFormatType.apng: 'png',
             StickerFormatType.lottie: 'json',
+            StickerFormatType.gif: 'gif',
         }
         # fmt: on
         return lookup[self]
