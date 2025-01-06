e6c9b88b597094e8083b3d86fed64971e24c0978
Kae Bartlett <kae@voxelfox.co.uk>
2025-01-06 20:48:46 +0000

    Fix circular import? I think?

--- novus/api/gateway/__init__.py
+++ novus/api/gateway/__init__.py
@@ -14,3 +14,10 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
+
+from .gateway import *
+
+__all__: tuple[str, ...] = (
+    "GatewayConnection",
+    "GatewayShard",
+)
